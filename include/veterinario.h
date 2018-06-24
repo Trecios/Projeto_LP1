7ced5cc2f24e57c6b27ca64bc04de2fedc317402
Bryan Brito <bhyanbrito+github@gmail.com>
2018-06-24 18:47:15 -0300

    Criado protótipo da classe veterinario.

---
+++ include/veterinario.h
@@ -0,0 +1,35 @@
+/**
+ * @file veterinario.h
+ * @brief Declaração da classe Funcionário e seus métodos.
+ * @author Bryan Brito
+ * @author Leonardo Matos
+ */
+#ifndef VETERINARIO_H
+#define VETERINARIO_H
+
+
+#include "funcionario.h"
+
+namespace PetFera{
+
+    /**
+     * @brief Classe Veterinário.
+     */
+    class Veterinario: public Funcionario{
+        public:
+
+            /**
+             * @brief Construtor da classe Veterinário.
+             */
+            Veterinario(int id_, std::string nome_, std::string cpf_, short idade_, short tipo_sanguineo_,
+                    char fator_rh_, std::sting especialidade_);
+
+            /**
+             * @brief Destrutor da classe Veterinário.
+             */
+            ~Veterinario();
+    }
+
+}
+
+#endif
