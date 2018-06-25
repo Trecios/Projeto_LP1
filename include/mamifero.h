932fab0ba11d5f094a6d95811c64d2926dbe31db
Bryan Brito <bhyanbrito+github@gmail.com>
2018-06-24 22:50:38 -0300

    Criado protótipo da classe mamífero.

---
+++ include/mamifero.h
@@ -0,0 +1,47 @@
+/**
+ * @file mamifero.h
+ * @brief Declaração da classe Mamífero e seus métodos.
+ * @author Bryan Brito
+ * @author Leonardo Matos
+ */
+
+#ifndef MAMIFERO_H
+#define MAMIFERO_H
+
+#include "Animal.h"
+
+using namespace std;
+
+namespace PetFera{
+
+    /**
+     * @brief Classe Anfíbio.
+     */
+    class Mamifero: public Animal{
+        protected:
+            string m_cor;
+
+        public:
+
+            /**
+             * @brief Construtor da classe Mamífero.
+             */
+            Mamifero(int id_, string classe_, string nome_, string cientifico_, char sexo_,
+                    float tamanho_, string dieta_, share_ptr<PetFera::Veterinario> veterinario_,
+                    share_ptr<PetFera::Tratador> tratador_, string batismo_, string cor_);
+
+            /**
+             * @brief Destrutor da classe Mamífero.
+             */
+            ~Mamifero();
+
+            /**
+             * @brief Método get do atributo m_cor.
+             * @return m_cor.
+             */
+            string getCor();
+    }
+
+}
+
+#endif
