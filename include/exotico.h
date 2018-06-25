e182206b13226948784a826f4711ceb9c1e0f241
Bryan Brito <bhyanbrito+github@gmail.com>
2018-06-25 00:22:32 -0300

    Criado protótipo da classe Exótico.

---
+++ include/exotico.h
@@ -0,0 +1,43 @@
+/**
+ * @file exotico.h
+ * @brief Declaração da classe Exótico e seus métodos.
+ * @author Bryan Brito
+ * @author Leonardo Matos
+ */
+
+#ifndef EXOTICO_H
+#define EXOTICO_H
+
+
+using namespace std;
+
+namespace PetFera{
+
+    /**
+     * @brief Classe Exótico.
+     */
+    class Exotico: public AnimalSilvestre{
+        protected:
+            string m_pais_origem;
+
+        public:
+
+            /**
+             * @brief Construtor da classe Exótico.
+             */
+            Exotico(string ibama_, string pais_origem_); 
+
+            /**
+             * @brief Destrutor da classe Exótico.
+             */
+            ~Exotico();
+
+            /**
+             * @brief Método get do atributo m_pais_origem.
+             * @return m_pais_origem.
+             */
+            string getPais();
+    }
+}
+
+#endif
