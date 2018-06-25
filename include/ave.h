a96063fa9850d5ffb4a46c5772d728871a011f49
Bryan Brito <bhyanbrito+github@gmail.com>
2018-06-24 23:48:53 -0300

    Criado protótipo da classe ave.

---
+++ include/ave.h
@@ -0,0 +1,54 @@
+/**
+ * @file ave.h
+ * @brief Declaração da classe Ave e seus métodos.
+ * @author Bryan Brito
+ * @author Leonardo Matos
+ */
+
+#ifndef AVE_H
+#define AVE_H
+
+#include "Animal.h"
+
+using namespace std;
+
+namespace PetFera{
+
+    /**
+     * @brief Classe Ave.
+     */
+    class Ave: public Animal{
+        protected:
+            int m_tamanho_bico;
+            int m_envergadura;
+
+        public:
+
+            /**
+             * @brief Construtor da classe Ave.
+             */
+            Ave(int id_, string classe_, string nome_, string cientifico_, char sexo_, 
+                    float tamanho_, string dieta_, share_ptr<PetFera::Veterinario> veterinario_,
+                    share_ptr<PetFera::Tratador> tratador_, string batismo_, int tamanho_bico_,
+                    int envergadura_);
+
+            /**
+             * @brief Destrutor da classe Ave.
+             */
+            ~Ave();
+
+            /**
+             * @brief Método get do atributo m_tamanho_bico.
+             * @return m_tamanho_bico.
+             */
+            bool getTamanhoBico();
+
+            /**
+             * @brief Método get do atributo m_envergadura.
+             * @return m_envergadura.
+             */
+            string getEnvergadura();
+    }
+}
+
+#endif
