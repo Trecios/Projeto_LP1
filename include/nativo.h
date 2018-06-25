d72294b20a35d902426428f030b3f1747501905b
Bryan Brito <bhyanbrito+github@gmail.com>
2018-06-25 00:18:13 -0300

    Criado protótipo da classe Nativo.

---
+++ include/nativo.h
@@ -0,0 +1,50 @@
+/**
+ * @file nativo.h
+ * @brief Declaração da classe Nativo e seus métodos.
+ * @author Bryan Brito
+ * @author Leonardo Matos
+ */
+
+#ifndef NATIVO_H
+#define NATIVO_H
+
+
+using namespace std;
+
+namespace PetFera{
+
+    /**
+     * @brief Classe Nativo.
+     */
+    class Nativo: public AnimalSilvestre{
+        protected:
+            string m_uf_origem;
+            string m_autorizacao;
+
+        public:
+
+            /**
+             * @brief Construtor da classe Nativo.
+             */
+            Nativo(string ibama_, string uf_origem_, string autorizacao_); 
+
+            /**
+             * @brief Destrutor da classe Nativo.
+             */
+            ~Nativo();
+
+            /**
+             * @brief Método get do atributo m_uf_origem.
+             * @return m_uf_origem.
+             */
+            string getOrigem();
+
+            /**
+             * @brief Método get do atributo m_autorizacao.
+             * @return m_uf_autorizacao.
+             */
+            string getAutorizacao();
+    }
+}
+
+#endif
